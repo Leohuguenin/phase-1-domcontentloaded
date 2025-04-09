7aa0bf3b54fb121a76343543620b5997ba43ae8a
Leohuguenin <leonardohuguenin@live.com>
2025-04-09 12:35:39 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    let paragraphElement = document.getElementById('text');
+    paragraphElement.textContent = "This is really cool!";
+})
